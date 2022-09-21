e766905f383d5d5c146389debc2fcbabb0f066c5
Felipe Jaramillo <felipejarher@gmail.com>
2022-09-21 17:12:41 +0200

    update

--- js/escenaBasica.js
+++ js/escenaBasica.js
@@ -34,7 +34,7 @@ function init()
 
     // Instanciar la camara
     camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,1,100);
-    camera.position.set(0.5,2,7);
+    camera.position.set(0.5,5,9);
     camera.lookAt(0,1,0);
 }
 
